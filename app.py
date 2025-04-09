1493bf334dcc616f91b516d236b65dbd3741db8d
Hem Tamang <hemmoktan9@gmail.com>
2025-04-09 22:46:58 +1000

    Adding all the code to deoply multi-container application

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+import psycopg2
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    try:
+        conn = psycopg2.connect(
+            host="db",
+            dbname="flaskdb"
+        )
+        return "✅ Connected to Postgres (no auth)"
+    except Exception as e:
+        return f"❌ Failed to connect: {e}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
